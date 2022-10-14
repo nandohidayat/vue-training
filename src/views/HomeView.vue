6cdf67eb5038ccd5cee407c9565f9ea37862b894
nandohidayat <mnandohidayat@gmail.com>
2022-10-14 18:30:09 +0700

    use vueuse

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,15 @@
 <template>
-  <div class="flex">
-    <box-card :box-model="box1" @selected="changeColor(box1)"></box-card>
-    <box-card :box-model="box2" @selected="changeColor(box2)"></box-card>
-    <box-card :box-model="box3" @selected="changeColor(box3)"></box-card>
-  </div>
+  <div>{{ coords.latitude }} - {{ coords.longitude }}</div>
+  <img :src="qrcode" alt="Location" />
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import BoxCard from "@/components/BoxCard.vue";
-import type { BoxModel } from "@/models/BoxModel";
+import { useTitle, useGeolocation } from "@vueuse/core";
+import { useQRCode } from "@vueuse/integrations/useQRCode";
 
-const box1 = reactive({
-  id: 1,
-  color: "red",
-});
-
-const box2 = reactive({
-  id: 2,
-  color: "blue",
-});
-
-const box3 = reactive({
-  id: 3,
-  color: "green",
-});
-
-const random_rgba = () => {
-  const o = Math.round,
-    r = Math.random,
-    s = 255;
-  return (
-    "rgba(" +
-    o(r() * s) +
-    "," +
-    o(r() * s) +
-    "," +
-    o(r() * s) +
-    "," +
-    r().toFixed(1) +
-    ")"
-  );
-};
-
-const changeColor = (boxModel: BoxModel) => {
-  boxModel.color = random_rgba();
-};
+useTitle("Vue is awesome!");
+const { coords } = useGeolocation();
+const qrcode = useQRCode(
+  `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+);
 </script>
-
-<style scoped></style>
